86210fe0011876883c924c04e0934d7390a64e28
Tabitha Wangechi <twangechi742@gmail.com>
2025-03-26 16:55:18 +0300

    my thirteenth commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchGalleries();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchGalleries();
+});
+
+function fetchGalleries() {
+  fetch("db.json")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.galleries) {
+        displayGalleries(data.galleries);
+      } else {
+        console.error("Galleries data not found in db.json");
+      }
+    })
+    .catch((error) => console.error("Error fetching galleries:", error));
+}
+
